c692ce53415cf2ea90a71ea3aeb17cc6575846b7
patel-zeel <patel_zeel@iitgn.ac.in>
2021-10-02 13:51:05 +0530

    fix

--- regdata/dataloaders/mcycle.py
+++ regdata/dataloaders/mcycle.py
@@ -5,7 +5,7 @@ class MotorcycleHelmet(Base):
                 mean_normalize_y=True, noisy=True, test_train_ratio=2, s_to_n_ratio=20,
                 noise_variance=None, scaler='std', random_state=0, backend=None):
 
-        name = 'mcycle.csv'
+        name = 'motorcycle_helmet.csv'
         X, y, Xnames, ynames = self.check_download_and_get(name)
 
         super().__init__(X, y, Xnames, ynames, return_test, scale_X, scale_y, 
